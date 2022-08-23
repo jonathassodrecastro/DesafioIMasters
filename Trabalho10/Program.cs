c6ef37a03ffc050c9f424c35a337f52f9405bdb3
Jonathas Sodré <jonathas_sl@hotmail.com>
2022-08-23 12:28:50 -0300

    Trabalhos 9 e 10

---
+++ Trabalho10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Trabalho10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string numero;
+            int a;
+            int tamanho;
+            
+            try
+            {
+                Console.WriteLine("Informe um número inteiro : ");
+                numero = Console.ReadLine();
+                tamanho = numero.Length;
+                a = int.Parse(numero);
+                p.calcular(a, tamanho);
+                Console.ReadKey();
+            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        private void calcular(int numero, int tamanho)
+        {
+            int a;
+            int soma = 0;
+            for (int i = 1; i <= tamanho; i++)
+            {
+                a = numero % 10;
+                numero = numero / 10;
+                Console.Write(a);
+                if (tamanho > i)
+                    Console.Write(" + ");
+                soma = soma + a;
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("A soma dos dígitos individuais deste número é => " + soma);
+        }
+    }
+}
---
+++ Trabalho9/Program.cs
@@ -0,0 +1,42 @@
+﻿// See using System;
+namespace Trabalho9
+{
+    class Program
+    {
+        public static void PorValor(int num)
+        {
+            num++;
+        }
+        public static void PorReferencia(ref int num)
+        {
+            num++;
+        }
+        static void Main(string[] args)
+        {
+            
+            int num;
+            try
+            {
+                Console.Write("Informe um número :\t");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\n\tTipo por Valor");
+                Console.WriteLine("--------------------------");
+                Console.Write("\nValor Anterior:\t{0}", num);
+                Program.PorValor(num);
+                Console.Write("\nValor Atual:\t{0}", num);
+                Console.WriteLine("\n\n\n--------------------");
+                Console.WriteLine("\tTipo por Referência");
+                Console.WriteLine("--------------------------");
+                Console.Write("\nValor Anterior:\t{0}", num);
+                Program.PorReferencia(ref num);
+                Console.Write("\nValor Atual:\t{0}", num);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
